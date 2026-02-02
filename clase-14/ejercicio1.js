cebfd5c723cb1ac46cb36b1a90da3cfc14a21d6f
Denise Maldonado <celeste41956@gmail.com>
2026-02-02 03:37:20 -0300

    Update ejercicio1.js

--- clase-14/ejercicio1.js
+++ clase-14/ejercicio1.js
@@ -1,8 +1,12 @@
 const prompt = require('prompt-sync')();
-// funcion flecha para calcular precio con iva
-const calcularPrecioConIva = (precio, porcentaje) => precio + (precio * porcentaje / 100);
-// ingreso de datos
-let precioProducto = parseFloat(prompt("ingresa el precio del producto: "));
-let porcentajeIva = parseFloat(prompt("ingresa el porcentaje de iva: "));
-// ejecucion
-console.log("el precio final es: " + calcularPrecioConIva(precioProducto, porcentajeIva));
+
+// funcion flecha para calcular total
+const calcularPrecioConIva = (valor, iva) => valor + (valor * iva / 100);
+
+// datos de entrada
+let costo = parseFloat(prompt("ingresa el precio del producto: "));
+let ivaIngresado = parseFloat(prompt("ingresa el porcentaje de iva: "));
+
+// salida
+let resultadoFinal = calcularPrecioConIva(costo, ivaIngresado);
+console.log("el precio final es: " + resultadoFinal);

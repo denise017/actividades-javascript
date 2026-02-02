d2361ee34016f8dd6939d893b6af9a00c62eb28b
Denise Maldonado <celeste41956@gmail.com>
2026-02-02 03:38:07 -0300

    update

--- clase-14/ejercicio2.js
+++ clase-14/ejercicio2.js
@@ -1,11 +1,13 @@
 const prompt = require('prompt-sync')();
 // funcion flecha para promedio
-const calcularPromedio = (n1, n2, n3, n4, n5) => (n1 + n2 + n3 + n4 + n5) / 5;
-// ingreso de datos
-let c1 = parseFloat(prompt("calificacion 1: "));
-let c2 = parseFloat(prompt("calificacion 2: "));
-let c3 = parseFloat(prompt("calificacion 3: "));
-let c4 = parseFloat(prompt("calificacion 4: "));
-let c5 = parseFloat(prompt("calificacion 5: "));
-// ejecucion
-console.log("el promedio es: " + calcularPromedio(c1, c2, c3, c4, c5));
+const calcularPromedio = (a, b, c, d, e) => (a + b + c + d + e) / 5;
+
+// entradas
+let notaA = parseFloat(prompt("nota 1: "));
+let notaB = parseFloat(prompt("nota 2: "));
+let notaC = parseFloat(prompt("nota 3: "));
+let notaD = parseFloat(prompt("nota 4: "));
+let notaE = parseFloat(prompt("nota 5: "));
+
+// salida
+console.log("el promedio es: " + calcularPromedio(notaA, notaB, notaC, notaD, notaE));
